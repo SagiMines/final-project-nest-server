36beeae7c61b939b8e908626737a526139a6d6b1
Sagi Mines <sagi1236@gmail.com>
2022-11-20 14:10:30 +0200

    Cookies domain fix

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       //expires in 1 year
-      cookie: {domain: 'netlify.app' ,secure: true, maxAge: 365*24*60*60*1000, httpOnly: false, sameSite: 'none'}, 
+      cookie: {domain: '.netlify.app' ,secure: true, maxAge: 365*24*60*60*1000, httpOnly: false, sameSite: 'none'}, 
     })
   )
   app.setGlobalPrefix('api')
