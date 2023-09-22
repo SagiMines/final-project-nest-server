6b4f66154f21fb7d1d70d3248db1676ddfbf68e6
Sagi Mines <sagi1236@gmail.com>
2023-09-22 14:43:28 +0300

    Updated the readme file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -12,61 +17,62 @@ import { TopProductsModule } from './top-products/top-products.module';
 import { WishlistModule } from './wishlist/wishlist.module';
 import { OrdersModule } from './orders/orders.module';
 import { OrderDetailsModule } from './order-details/order-details.module';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import { IsUserExistMiddleware } from './middlewares/is-user-connected.middleware';
 import { CartModule } from './cart/cart.module';
 import { LogOutUserMiddleware } from './middlewares/log-out-user.middleware';
 import { VerifyMailMiddleware } from './middlewares/verify-mail.middleware';
 import { GetUserMessageMiddleware } from './middlewares/get-user-message.middleware';
-dotenv.config()
+dotenv.config();
 @Module({
   imports: [
-     ConfigModule.forRoot({
-    isGlobal: true
-  }), ProductsModule, UsersModule, CategoriesModule, ProductImagesModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: process.env.HOST,
-    port: 3306,
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: false
-  }), TopProductsModule, WishlistModule, OrdersModule, OrderDetailsModule, CartModule,],
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductsModule,
+    UsersModule,
+    CategoriesModule,
+    ProductImagesModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPE,
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    TopProductsModule,
+    WishlistModule,
+    OrdersModule,
+    OrderDetailsModule,
+    CartModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AddUserMiddleware).forRoutes(
-      {
-        path:'/register',
-        method: RequestMethod.POST
-      }
-    ),
-    consumer.apply(IsUserExistMiddleware).forRoutes(
-      {
-        path:'/login/:encryptedUserId',
-        method: RequestMethod.GET
-      }
-    ),
-    consumer.apply(LogOutUserMiddleware).forRoutes(
-      {
-        path:'/logout',
-        method: RequestMethod.POST
-      }
-    ),
-    consumer.apply(VerifyMailMiddleware).forRoutes(
-      {
-        path:'/register',
-        method: RequestMethod.GET
-      }
-    ),
-    consumer.apply(GetUserMessageMiddleware).forRoutes(
-      {
-        path:'/contact-us',
-        method: RequestMethod.POST
-      }
-    )
+    consumer.apply(AddUserMiddleware).forRoutes({
+      path: '/register',
+      method: RequestMethod.POST,
+    }),
+      consumer.apply(IsUserExistMiddleware).forRoutes({
+        path: '/login/:encryptedUserId',
+        method: RequestMethod.GET,
+      }),
+      consumer.apply(LogOutUserMiddleware).forRoutes({
+        path: '/logout',
+        method: RequestMethod.POST,
+      }),
+      consumer.apply(VerifyMailMiddleware).forRoutes({
+        path: '/register',
+        method: RequestMethod.GET,
+      }),
+      consumer.apply(GetUserMessageMiddleware).forRoutes({
+        path: '/contact-us',
+        method: RequestMethod.POST,
+      });
   }
 }
